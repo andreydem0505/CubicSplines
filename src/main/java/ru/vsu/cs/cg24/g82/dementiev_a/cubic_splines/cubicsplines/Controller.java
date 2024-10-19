035eaf821314459b256fb0564b7a83757a042678
Andrey <andreydem42@gmail.com>
2024-10-19 23:41:27 +0300

    flexible parameter change

--- src/main/java/ru/vsu/cs/cg24/g82/dementiev_a/cubic_splines/cubicsplines/Controller.java
+++ src/main/java/ru/vsu/cs/cg24/g82/dementiev_a/cubic_splines/cubicsplines/Controller.java
@@ -2,8 +2,10 @@ package ru.vsu.cs.cg24.g82.dementiev_a.cubic_splines.cubicsplines;
 
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,14 +16,21 @@ public class Controller {
     AnchorPane anchorPane;
     @FXML
     private Canvas canvas;
+    @FXML
+    private Text text;
+    @FXML
+    private Button button;
 
     private Painter painter;
     private List<Point> points;
     private final int POINT_RADIUS = 3;
     private Point dragged = null;
+    private final double[] STEPS = new double[] {0, 1, 0.5, 0.05, 0.005};
+    private int stepPointer;
 
     @FXML
     private void initialize() {
+        stepPointer = 0;
         anchorPane.widthProperty().addListener((ov, oldValue, newValue) -> {
             canvas.setWidth(newValue.doubleValue());
             repaint();
@@ -56,16 +65,32 @@ public class Controller {
             dragged.setY((int) e.getY());
             repaint();
         });
+
+        button.setOnMouseClicked(mouseEvent -> {
+            stepPointer = (stepPointer + 1) % STEPS.length;
+            button.setText("Step: " + (STEPS[stepPointer] == 0 ? "flexible" : String.valueOf(STEPS[stepPointer])));
+            repaint();
+        });
     }
 
     private void repaint() {
         painter.clear();
 
-        int[] x = new int[points.size()];
-        int[] y = new int[points.size()];
-        for (int i = 0; i < points.size(); i++) {
-            x[i] = points.get(i).getX();
-            y[i] = points.get(i).getY();
+        // Не рисуем точку, которая на тех же координатах, что и прошлая
+        List<Point> pointsToDraw = new ArrayList<>();
+        if (!points.isEmpty())
+            pointsToDraw.add(points.get(0));
+        for (int i = 1; i < points.size(); i++) {
+            if (points.get(i).getX() != points.get(i - 1).getX() || points.get(i).getY() != points.get(i - 1).getY()) {
+                pointsToDraw.add(points.get(i));
+            }
+        }
+
+        int[] x = new int[pointsToDraw.size()];
+        int[] y = new int[pointsToDraw.size()];
+        for (int i = 0; i < pointsToDraw.size(); i++) {
+            x[i] = pointsToDraw.get(i).getX();
+            y[i] = pointsToDraw.get(i).getY();
             painter.drawPoint(x[i], y[i], POINT_RADIUS);
         }
         // Считаем параметр для каждой точки
@@ -75,10 +100,43 @@ public class Controller {
         Spline[] xSplines = Solver.getSplines(t, x);
         Spline[] ySplines = Solver.getSplines(t, y);
 
-        for (int i = 0; i < xSplines.length; i++) {
-            for (double p = t[i]; p <= t[i + 1]; p += 0.05) {
-                painter.putPixel((int) xSplines[i].calculate(p), (int) ySplines[i].calculate(p));
+        long computations = 0;
+        long start = System.currentTimeMillis();
+
+        if (STEPS[stepPointer] == 0 && xSplines.length > 0) {
+            // Параметр перебирается динамически
+            double lastP = 0;
+            int lastX = (int) Math.round(xSplines[0].calculate(0));
+            int lastY = (int) Math.round(ySplines[0].calculate(0));
+            int curX, curY, counter = 0;
+            for (int i = 0; i < xSplines.length; i++) {
+                for (double p = t[i]; p <= t[i + 1]; p += 1) {
+                    curX = (int) Math.round(xSplines[i].calculate(p));
+                    curY = (int) Math.round(ySplines[i].calculate(p));
+                    painter.putPixel(curX, curY);
+                    if (Math.abs(curX - lastX) + Math.abs(curY - lastY) >= 2 && counter <= 5) {
+                        p = (lastP + p) / 2 - 1;
+                        counter++;
+                    } else {
+                        lastX = curX;
+                        lastY = curY;
+                        lastP = p;
+                        counter = 0;
+                    }
+                    computations++;
+                }
+            }
+        } else {
+            // Параметр перебирается с определенным шагом
+            for (int i = 0; i < xSplines.length; i++) {
+                for (double p = t[i]; p <= t[i + 1]; p += STEPS[stepPointer]) {
+                    painter.putPixel((int) Math.round(xSplines[i].calculate(p)), (int) Math.round(ySplines[i].calculate(p)));
+                    computations++;
+                }
             }
         }
+
+        long end = System.currentTimeMillis();
+        text.setText("Computations: " + computations + "\nTime: " + (end - start) + "ms");
     }
 }
